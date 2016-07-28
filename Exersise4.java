955cb8f9d3cdce32889d5c4f49e3f1373a2be99f
JRShaw1 <james.shaw@qa.com>
2016-07-28 14:35:06 +0100

    New updates

--- Exersise4.java
+++ Exersise4.java
@@ -4,69 +4,84 @@ public class Exersise4 {
 
 	public static void main(String[] args) {
 
-
 		Board board = new Board();
 
 		int[][] grid = board.Board();
-		
-		Ship PatrolP1 = new Ship();    //direction, size, x-corad, y-coard
-		int X = PatrolP1.X(1);
-		int Y = PatrolP1.Y(1);
-		int direction = PatrolP1.Direction(0);
-		int size = PatrolP1.Size(1);             // how long after the inital point, I.E. a patrol boat is two long so it's size is 1 as the initial point is already plotted.
-		
-	/*	Ship PatrolP2 = new Ship();
-		int X = PatrolP2.X(2);
-		int Y = PatrolP2.Y(2);
-		int direction = PatrolP2.Direction(2);
-	*/	
 
-		
-		
-		
-		if(direction == 0){
-		grid[Y-size][X] = 1;
-		grid[Y][X] =1;
-		}
+		for (int i = 0; i < 2; i++) {
 
-		else if(direction == 1){
-		grid[Y+size][X] = 1;
-		grid[Y][X] =1;
-		}
+			if (i % 2 <= 0) {
 
-		else if(direction == 2){
-		grid[Y][X-size] = 1;
-		grid[Y][X] =1;
-		}
+				Ship Patrol = new Ship();                        // direction, size, x-corad, y-coard
+				int X = Patrol.X(1);                            //
+				int Y = Patrol.Y(1);                           //
+				int direction = Patrol.Direction(0);          //
+				int size = Patrol.Size(1);                   // how long after the inital point,
+											                // I.E. a patrol boat is two long so
+											               // it's size is 1 as the initial
+											              // point is already plotted.
 
-		else{
-		grid[Y][X+size] = 1;
-		grid[Y][X] = 1;
-		}
-		
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-
-				System.out.print(grid[i][j]);
-			}
-			System.out.print("\n");
-		}
-		
-/*
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-
-				if (i == X && j == Y || i == W && j == V) {
-					grid[i][j] = 1;
-				} else {
-					grid[i][j] = 0;
+				if (direction == 0) {
+					grid[Y - size][X] = 1;
+					grid[Y][X] = 1;
 				}
+
+				else if (direction == 1) {
+					grid[Y + size][X] = 1;
+					grid[Y][X] = 1;
+				}
+
+				else if (direction == 2) {
+					grid[Y][X - size] = 1;
+					grid[Y][X] = 1;
+				}
+
+				else {
+					grid[Y][X + size] = 1;
+					grid[Y][X] = 1;
+				}
+
+			} else {
+
+				Ship Patrol = new Ship();
+				int X = Patrol.X(2);
+				int Y = Patrol.Y(2);
+				int direction = Patrol.Direction(2);
+				int size = Patrol.Size(1);                       // how long after the inital point,
+											                    // I.E. a patrol boat is two long so
+											                   // it's size is 1 as the initial
+											                  // point is already plotted.
+
+				if (direction == 0) {
+					grid[Y - size][X] = 2;
+					grid[Y][X] = 2;
+				}
+
+				else if (direction == 1) {
+					grid[Y + size][X] = 2;
+					grid[Y][X] = 2;
+				}
+
+				else if (direction == 2) {
+					grid[Y][X - size] = 2;
+					grid[Y][X] = 2;
+				}
+
+				else {
+					grid[Y][X + size] = 2;
+					grid[Y][X] = 2;
+				}
+			}
+		}
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+
 				System.out.print(grid[i][j]);
 			}
 			System.out.print("\n");
 		}
-*/
+
 	}
 
 }
-
