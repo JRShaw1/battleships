caca7f72ffcbe90b232c5d7146c62fc16976a8bc
JRShaw1 <james.shaw@qa.com>
2016-07-28 15:49:45 +0100

    Update

--- Exersise4.java
+++ Exersise4.java
@@ -4,82 +4,108 @@ public class Exersise4 {
 
 	public static void main(String[] args) {
 
-		Board board = new Board();
+		for (int k = 0; k < 2; k++) {
 
-		int[][] grid = board.Board();
+			if (k % 2 <= 0) {
 
-		for (int i = 0; i < 2; i++) {
+				Board board = new Board();
 
-			if (i % 2 <= 0) {
+				int[][] grid = board.Board();
 
-				Ship Patrol = new Ship();                        // direction, size, x-corad, y-coard
-				int X = Patrol.X(1);                            //
-				int Y = Patrol.Y(1);                           //
-				int direction = Patrol.Direction(0);          //
-				int size = Patrol.Size(1);                   // how long after the inital point,
-											                // I.E. a patrol boat is two long so
-											               // it's size is 1 as the initial
-											              // point is already plotted.
+				Ship Patrol = new Ship(); // direction, size, x-corad, y-coard
+				int X = Patrol.X(1); //
+				int Y = Patrol.Y(1); //
+				int direction = Patrol.Direction(0); //
+				int size = Patrol.Size(1); // how long after the inital point,
+											// I.E. a patrol boat is two long so
+											// it's size is 1 as the initial
+											// point is already plotted.
 
-				if (direction == 0) {
+				if (direction == 0 && grid[Y][X] == 0 && grid[Y - size][X] <= 0) {
 					grid[Y - size][X] = 1;
 					grid[Y][X] = 1;
 				}
 
-				else if (direction == 1) {
+				else if (direction == 1 && grid[Y][X] == 0
+						&& grid[Y + size][X] <= 0) {
 					grid[Y + size][X] = 1;
 					grid[Y][X] = 1;
 				}
 
-				else if (direction == 2) {
+				else if (direction == 2 && grid[Y][X] == 0
+						&& grid[Y][X - size] <= 0) {
 					grid[Y][X - size] = 1;
 					grid[Y][X] = 1;
 				}
 
-				else {
+				else if (direction == 3 && grid[Y][X] == 0
+						&& grid[Y][X + size] <= 0) {
 					grid[Y][X + size] = 1;
 					grid[Y][X] = 1;
+				} else {
+					System.out.println("Can't put a ship there");
 				}
 
+				for (int i = 0; i < 3; i++) {
+					for (int j = 0; j < 3; j++) {
+
+						System.out.print(grid[i][j]);
+						System.out.print(" ");
+					}
+					System.out.print("\n");
+
+				}
+				System.out.print("\n");
 			} else {
+
+				Board board = new Board();
+
+				int[][] grid = board.Board();
 
 				Ship Patrol = new Ship();
 				int X = Patrol.X(2);
 				int Y = Patrol.Y(2);
 				int direction = Patrol.Direction(2);
-				int size = Patrol.Size(1);                       // how long after the inital point,
-											                    // I.E. a patrol boat is two long so
-											                   // it's size is 1 as the initial
-											                  // point is already plotted.
+				int size = Patrol.Size(1); // how long after the inital point,
+											// I.E. a patrol boat is two long so
+											// it's size is 1 as the initial
+											// point is already plotted.
 
-				if (direction == 0) {
-					grid[Y - size][X] = 2;
-					grid[Y][X] = 2;
+				if (direction == 0 && grid[Y][X] == 0 && grid[Y - size][X] <= 0) {
+					grid[Y - size][X] = 1;
+					grid[Y][X] = 1;
 				}
 
-				else if (direction == 1) {
-					grid[Y + size][X] = 2;
-					grid[Y][X] = 2;
+				else if (direction == 1 && grid[Y][X] == 0
+						&& grid[Y + size][X] <= 0) {
+					grid[Y + size][X] = 1;
+					grid[Y][X] = 1;
 				}
 
-				else if (direction == 2) {
-					grid[Y][X - size] = 2;
-					grid[Y][X] = 2;
+				else if (direction == 2 && grid[Y][X] == 0
+						&& grid[Y][X - size] <= 0) {
+					grid[Y][X - size] = 1;
+					grid[Y][X] = 1;
 				}
 
-				else {
-					grid[Y][X + size] = 2;
-					grid[Y][X] = 2;
+				else if (direction == 3 && grid[Y][X] == 0
+						&& grid[Y][X + size] <= 0) {
+					grid[Y][X + size] = 1;
+					grid[Y][X] = 1;
+				} else {
+					System.out.println("Can't put a ship there");
+				}
+
+				for (int i = 0; i < 3; i++) {
+					for (int j = 0; j < 3; j++) {
+
+						System.out.print(grid[i][j]);
+						System.out.print(" ");
+					}
+					System.out.print("\n");
 				}
 			}
-		}
 
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-
-				System.out.print(grid[i][j]);
-			}
-			System.out.print("\n");
 		}
 
 	}
